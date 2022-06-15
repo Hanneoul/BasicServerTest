4a07d0828cdcd6b3f72b03c4f6c88be522bca83a
PC2010221\YUHAN <e>
2022-06-15 22:54:41 +0900

    초기 커밋.

---
+++ BasicServerTest/FileLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Lecture15
+{
+    class FileLogger
+    {
+        public string _fileName { get; set; }
+
+        public FileLogger(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public void Write(string logMessage)
+        {
+            StreamWriter writer;
+            writer = File.CreateText(_fileName);        //Text File이 저장될 위치(파일명)
+                                                        //파일 이름만 지정하면 컴파일된 폴더내 해당 파일 이름으로 저장됨
+            writer.WriteLine(logMessage);               //저장될 string
+            writer.Close();
+        }
+
+        //public void Store(Settings settings, string fileName)
+        //{
+        //    // Serialize
+        //    string jsonString = JsonSerializer.Serialize(settings);
+        //    System.Console.WriteLine(jsonString);
+        //    StreamWriter writer;
+        //    writer = File.CreateText(fileName);        //Text File이 저장될 위치(파일명)
+        //                                               //파일 이름만 지정하면 컴파일된 폴더내 해당 파일 이름으로 저장됨
+        //    writer.WriteLine(jsonString);    //저장될 string
+        //    writer.Close();
+        //}
+        //public static Settings Load(string fileName)
+        //{
+        //    string jsonString = File.ReadAllText(fileName);
+        //    return JsonSerializer.Deserialize<Settings>(jsonString);
+        //}
+    }
+}
---
+++ BasicServerTest/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Net;
+using System.Threading;
+using System.Text.Json;
+
+
+namespace FinalTestSample
+{
+
+
+    public class Listener
+    {
+    }
+
+    public class ServerLoop
+    {
+        HttpListener _listener;
+        string _address;
+        public ServerLoop(string address, string port)
+        {
+            _listener = new HttpListener();
+            _address = "http://127.0.0.1" + ":" + port + "/";
+            
+        }
+
+        public void AddRestAPI(string apiName)
+        {
+            _listener.Prefixes.Add(_address + apiName + "/");
+        }
+        public void Run()
+        {
+            _listener.Start();
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+
+            }
+        }
+
+        public void Close()
+        {
+            _listener.Stop();
+        }
+    }
+
+    public class HttpService
+    {
+        HttpService(HttpListener _listener)
+        {
+            HttpListenerContext context = _listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+        }
+
+        HttpListenerContext context;
+        HttpListenerRequest request;
+        HttpListenerResponse response;
+
+    }
+
+    public class Program
+    {
+        public static int Main()
+        {
+            ServerLoop server = new ServerLoop("http://127.0.0.1","3000");
+            server.Prefixes.Add("GodDamn");
+            server.Prefixes.Add("GodDamn");
+            server.Prefixes.Add("GodDamn");
+            server.Prefixes.Add("GodDamn");
+            server.Prefixes.Add("GodDamn");
+            server.Run();
+
+
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+            response.AddHeader("Access-Control-Allow-Origin", "*");
+            Stream body = request.InputStream;
+            System.Text.Encoding encoding = request.ContentEncoding;
+            System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+
+            string rawurl = request.RawUrl;
+            string httpmethod = request.HttpMethod;
+
+            string result = "";
+
+            result += string.Format("httpmethod = {0}\r\n", httpmethod);
+            result += string.Format("rawurl = {0}\r\n", rawurl);
+            System.Console.WriteLine(result);
+
+            soo s = new soo();
+            s.body = "MaryJane";
+
+            string responseString = JsonSerializer.Serialize(s);
+            System.Console.WriteLine(responseString);
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            response.OutputStream.Close();
+
+
+            return 0;
+        }
+    }
+}
